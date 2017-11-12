08a34f33320da569b01d02de6364d86bf136e128
AKSHAY JAITLY <akshay365@icloud.com>
2017-11-11 19:31:41 -0500

    Update qualification.js

--- js/qualification.js
+++ js/qualification.js
@@ -57,23 +57,6 @@ function updateDisplay(list){
     displayedList=list;
 }
 
-var $filterableRows = $('qitemstable').find('tr').not(':first'),
-        $inputs = $('.search-key');
-
-$inputs.on('input', function() {
-
-    $filterableRows.hide().filter(function() {
-    return $(this).find('td').filter(function() {
-        
-      var tdText = $(this).text().toLowerCase(),
-            inputValue = $('#' + $(this).data('input')).val().toLowerCase();
-    
-        return tdText.indexOf(inputValue) != -1;
-    
-    }).length == $(this).find('td').length;
-  }).show();
-
-});
 
 function checkAll(source){
     boxes = document.getElementsByClassName("checkbox");
